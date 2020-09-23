c3c3797adc5e59eefeaac4695b656ea9b30abe5f
Hminji <49913441+J-petto@users.noreply.github.com>
2020-09-23 16:06:42 +0900

    mysql 연결 구조 수정, dotenv

--- express-main-example/index.js
+++ express-main-example/index.js
@@ -1,6 +1,9 @@
 const app = require('./express/app');
 const sequelize = require('./sequelize');
-const PORT = 8080;
+const PORT = process.env.PORT;
+const dotenv = require('dotenv');
+dotenv.config();
+
 
 async function assertDatabaseConnectionOk() {
 	console.log(`Checking database connection...`);
